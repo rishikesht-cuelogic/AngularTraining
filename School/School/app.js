02deb3d6ed38761eafad1cbe6e0de64dbc8b4ede
darshanc-cuelogic <rishikesh.tadka@cuelogic.co.in>
2016-03-30 16:02:31 +0530

    Initial Commit

---
+++ School/School/app.js
@@ -0,0 +1,1 @@
+﻿var school = angular.module('school', []);
---
+++ School/School/app/binding/binding.js
@@ -0,0 +1,7 @@
+﻿school.controller('bindingController', ['$scope', function ($scope) {
+    $scope.firstName = "Nilesh";
+    $scope.lastName = "Patil";
+    $scope.fullName = function () {
+        return $scope.firstName +' '+ $scope.lastName;
+    }    
+}]);
